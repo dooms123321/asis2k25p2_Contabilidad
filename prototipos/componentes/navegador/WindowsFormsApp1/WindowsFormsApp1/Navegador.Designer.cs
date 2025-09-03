98293a4f5dae09dc7719ed8428505729ea1272fe
Pedro Ibañez <pibanezl@miumg.edu.gt>
2025-09-02 18:04:47 -0600

    Agregacion de Iconos

--- prototipos/componentes/navegador/WindowsFormsApp1/WindowsFormsApp1/Navegador.Designer.cs
+++ prototipos/componentes/navegador/WindowsFormsApp1/WindowsFormsApp1/Navegador.Designer.cs
@@ -29,6 +29,7 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Navegador));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -65,18 +66,16 @@ namespace WindowsFormsApp1
             this.flowLayoutPanel1.Controls.Add(this.button14);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1516, 116);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1152, 94);
             this.flowLayoutPanel1.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.Image = global::WindowsFormsApp1.Properties.Resources.add_insert_new_plus_button_icon_142943;
-            this.button1.Location = new System.Drawing.Point(4, 4);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 107);
+            this.button1.Size = new System.Drawing.Size(74, 87);
             this.button1.TabIndex = 0;
             this.button1.Text = "ingresar";
             this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -85,10 +84,9 @@ namespace WindowsFormsApp1
             // button2
             // 
             this.button2.Image = global::WindowsFormsApp1.Properties.Resources.compose_edit_modify_icon_177770;
-            this.button2.Location = new System.Drawing.Point(111, 4);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(83, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 107);
+            this.button2.Size = new System.Drawing.Size(74, 87);
             this.button2.TabIndex = 1;
             this.button2.Text = "Modificar";
             this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -97,10 +95,9 @@ namespace WindowsFormsApp1
             // button3
             // 
             this.button3.Image = global::WindowsFormsApp1.Properties.Resources.savetheapplication_guardar_2958;
-            this.button3.Location = new System.Drawing.Point(218, 4);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(163, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 107);
+            this.button3.Size = new System.Drawing.Size(74, 87);
             this.button3.TabIndex = 2;
             this.button3.Text = "Guardar";
             this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -108,10 +105,10 @@ namespace WindowsFormsApp1
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(325, 4);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(243, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(99, 107);
+            this.button4.Size = new System.Drawing.Size(74, 87);
             this.button4.TabIndex = 3;
             this.button4.Text = "Cancelar";
             this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -120,10 +117,9 @@ namespace WindowsFormsApp1
             // button5
             // 
             this.button5.Image = global::WindowsFormsApp1.Properties.Resources.delete_remove_trash_icon_177304;
-            this.button5.Location = new System.Drawing.Point(432, 4);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Location = new System.Drawing.Point(323, 3);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(99, 107);
+            this.button5.Size = new System.Drawing.Size(74, 87);
             this.button5.TabIndex = 4;
             this.button5.Text = "Eliminar";
             this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -131,10 +127,10 @@ namespace WindowsFormsApp1
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(539, 4);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(403, 3);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(99, 107);
+            this.button6.Size = new System.Drawing.Size(74, 87);
             this.button6.TabIndex = 5;
             this.button6.Text = "Consultar";
             this.button6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -142,10 +138,10 @@ namespace WindowsFormsApp1
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(646, 4);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(483, 3);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(99, 107);
+            this.button7.Size = new System.Drawing.Size(74, 87);
             this.button7.TabIndex = 6;
             this.button7.Text = "Imprimir";
             this.button7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -153,10 +149,10 @@ namespace WindowsFormsApp1
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(753, 4);
-            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
+            this.button8.Location = new System.Drawing.Point(563, 3);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(99, 107);
+            this.button8.Size = new System.Drawing.Size(74, 87);
             this.button8.TabIndex = 7;
             this.button8.Text = "Refrescar";
             this.button8.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -164,10 +160,10 @@ namespace WindowsFormsApp1
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(860, 4);
-            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button9.Image = ((System.Drawing.Image)(resources.GetObject("button9.Image")));
+            this.button9.Location = new System.Drawing.Point(643, 3);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(99, 107);
+            this.button9.Size = new System.Drawing.Size(74, 87);
             this.button9.TabIndex = 8;
             this.button9.Text = "Inicio";
             this.button9.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -175,10 +171,10 @@ namespace WindowsFormsApp1
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(967, 4);
-            this.button10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button10.Image = ((System.Drawing.Image)(resources.GetObject("button10.Image")));
+            this.button10.Location = new System.Drawing.Point(723, 3);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(99, 107);
+            this.button10.Size = new System.Drawing.Size(74, 87);
             this.button10.TabIndex = 9;
             this.button10.Text = "Anterior";
             this.button10.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -186,10 +182,10 @@ namespace WindowsFormsApp1
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(1074, 4);
-            this.button11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button11.Image = ((System.Drawing.Image)(resources.GetObject("button11.Image")));
+            this.button11.Location = new System.Drawing.Point(803, 3);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(99, 107);
+            this.button11.Size = new System.Drawing.Size(74, 87);
             this.button11.TabIndex = 10;
             this.button11.Text = "Siguiente";
             this.button11.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -197,10 +193,10 @@ namespace WindowsFormsApp1
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(1181, 4);
-            this.button12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button12.Image = ((System.Drawing.Image)(resources.GetObject("button12.Image")));
+            this.button12.Location = new System.Drawing.Point(883, 3);
             this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(99, 107);
+            this.button12.Size = new System.Drawing.Size(74, 87);
             this.button12.TabIndex = 11;
             this.button12.Text = "Fin";
             this.button12.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -208,10 +204,10 @@ namespace WindowsFormsApp1
             // 
             // button13
             // 
-            this.button13.Location = new System.Drawing.Point(1288, 4);
-            this.button13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button13.Image = ((System.Drawing.Image)(resources.GetObject("button13.Image")));
+            this.button13.Location = new System.Drawing.Point(963, 3);
             this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(99, 107);
+            this.button13.Size = new System.Drawing.Size(74, 87);
             this.button13.TabIndex = 12;
             this.button13.Text = "Ayuda";
             this.button13.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -219,10 +215,10 @@ namespace WindowsFormsApp1
             // 
             // button14
             // 
-            this.button14.Location = new System.Drawing.Point(1395, 4);
-            this.button14.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button14.Image = ((System.Drawing.Image)(resources.GetObject("button14.Image")));
+            this.button14.Location = new System.Drawing.Point(1043, 3);
             this.button14.Name = "button14";
-            this.button14.Size = new System.Drawing.Size(99, 107);
+            this.button14.Size = new System.Drawing.Size(74, 87);
             this.button14.TabIndex = 13;
             this.button14.Text = "Salir";
             this.button14.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -230,12 +226,11 @@ namespace WindowsFormsApp1
             // 
             // Navegador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Navegador";
-            this.Size = new System.Drawing.Size(1516, 390);
+            this.Size = new System.Drawing.Size(1152, 417);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
