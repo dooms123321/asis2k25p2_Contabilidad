d0b43408f36fefb475b3dfbbad0881e66ad350f7
DiegoSaquil <157521877+DiegoSaquil@users.noreply.github.com>
2025-09-06 11:31:01 -0600

    Pruebas y clase conexion

---
+++ codigo/componentes/consultas/Componente_Consultas/Capa_Modelo_Componente_Consultas/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Modelo_Componente_Consultas
+{
+    class Conexion
+    {
+        //Esto se utiliza para conexiòn en base de datos.
+    }
+}
