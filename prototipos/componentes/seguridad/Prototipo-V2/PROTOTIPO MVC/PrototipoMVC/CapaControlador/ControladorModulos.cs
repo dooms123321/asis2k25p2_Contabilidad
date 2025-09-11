2b54cfd16f3e990528a3465fa56ca7fc30e4fde0
dooms123321 <dmazariegosd4@miumg.edu.gt>
2025-09-11 11:55:55 -0600

    modulos corregidos

---
+++ prototipos/componentes/seguridad/Prototipo-V2/PROTOTIPO MVC/PrototipoMVC/CapaControlador/ControladorModulos.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using CapaModelo;
+
+namespace CapaControlador
+{
+    public class ControladorModulos
+    {
+        SentenciasModulos snm = new SentenciasModulos();
+
+        public string[] ItemsModulos()
+        {
+            return snm.LlenarComboModulos();
+        }
+
+        public DataTable ObtenerModulos()
+        {
+            return snm.ObtenerModulos();
+        }
+
+        public DataRow BuscarModulo(int idModulo)
+        {
+            return snm.BuscarModuloPorId(idModulo);
+        }
+
+        public bool EliminarModulo(int id)
+        {
+            int filas = snm.EliminarModulo(id);
+            return filas > 0;
+        }
+
+        public bool InsertarModulo(int id, string nombre, string descripcion, byte estado)
+        {
+            int filas = snm.InsertarModulo(id, nombre, descripcion, estado);
+            return filas > 0;
+        }
+
+        public bool ModificarModulo(int id, string nombre, string descripcion, byte estado)
+        {
+            int filas = snm.ModificarModulo(id, nombre, descripcion, estado);
+            return filas > 0;
+        }
+    }
+}
