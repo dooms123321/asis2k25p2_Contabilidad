c989aa4137d13333bc71e4a640081dbe69bdc0c6
Sergio Emmanuel Izeppi Valle <sergioizeppi@gmail.com>
2025-09-12 09:46:27 -0600

    implementacion de eliminacion y obtencion reportes

--- codigo/componentes/reporteador/reporteador/Capa_Controlador_Reporteador/Controlador_Reporteador.cs
+++ codigo/componentes/reporteador/reporteador/Capa_Controlador_Reporteador/Controlador_Reporteador.cs
@@ -26,8 +26,18 @@ namespace Capa_Controlador_Reporteador
         // fin de código de: Paula Leonardo con carné: 0901-22-9580 en la fecha de: 11/09/2025
 
 
+        //inicio de codigo Sergio Izeppi, Carne: 0901-22-8946 en la fecha 11/09/2025
 
+        public void EliminarReporte(int id)
+        {
+            sentencias.EliminarReporte(id);
+        }
 
+        public DataTable ObtenerReportes()
+        {
+            return sentencias.ObtenerReporte();
+        }
+        //fin codigo Sergio
 
     }
 }
