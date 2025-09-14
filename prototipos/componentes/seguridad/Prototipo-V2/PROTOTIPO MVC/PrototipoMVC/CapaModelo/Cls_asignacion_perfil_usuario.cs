84438dc5c009fe8e75f18d314f41c3884150015a
Marcos <marcosvelasquez447@gmail.com>
2025-09-14 16:01:30 -0600

    Conexion Formulario Asignacion

---
+++ prototipos/componentes/seguridad/Prototipo-V2/PROTOTIPO MVC/PrototipoMVC/CapaModelo/Cls_asignacion_perfil_usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/* Brandon Alexander Hernandez Salguero
+ * 0901-22-9663
+ */
+
+namespace CapaModelo
+{
+    public class Cls_asignacion_perfil_usuario
+    {
+        public int fk_id_usuario { get; set; }
+        public int fk_id_perfil { get; set; }
+
+        public Cls_asignacion_perfil_usuario() { }
+
+        public Cls_asignacion_perfil_usuario(int fk_id_usuario, int fk_id_perfil)
+        {
+            this.fk_id_usuario = fk_id_usuario;
+            this.fk_id_perfil = fk_id_perfil;
+        }
+    }
+}
