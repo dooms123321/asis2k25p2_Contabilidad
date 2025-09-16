413476b9a3aada5b386ec366bf0deecace8cfd24
dooms123321 <dmazariegosd4@miumg.edu.gt>
2025-09-16 17:15:14 -0600

    Backup de la rama main con todos los cambios locales

---
+++ prototipos/componentes/seguridad/Prototipo-V2/PROTOTIPO MVC/PrototipoMVC/CapaVista/frmSlash.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class frmSlash : Form
+    {
+        public frmSlash()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSlash_Load(object sender, EventArgs e)
+        {
+            // Configuración del PictureBox
+            Pic_carga.Dock = DockStyle.Fill;          // Que ocupe todo el formulario
+            Pic_carga.SizeMode = PictureBoxSizeMode.Zoom; // Mantiene proporción del GIF
+            Pic_carga.SendToBack();                  // Lo manda al fondo para no tapar la barra ni el label
+
+            // Inicializa la barra en 0
+            Pgb_carga.Value = 0;
+
+            // Configura el Timer
+            Tmr_carga.Interval = 100; // 100 ms
+            Tmr_carga.Enabled = true;
+            Tmr_carga.Start();
+        }
+
+        private void Tmr_carga_Tick(object sender, EventArgs e)
+        {
+            // Avanza la barra
+            if (Pgb_carga.Value < Pgb_carga.Maximum)
+            {
+                Pgb_carga.Increment(2);
+                Lbl_carga.Text = Pgb_carga.Value.ToString() + "%";
+            }
+
+            // Cuando llega al máximo
+            if (Pgb_carga.Value >= Pgb_carga.Maximum)
+            {
+                Tmr_carga.Stop();
+
+                // Oculta el splash
+                this.Hide();
+
+                // Abre el login
+                frmLogin frmLogin = new frmLogin();
+                frmLogin.ShowDialog();
+
+                // Cuando cierres el login, termina la aplicación
+                Application.Exit();
+            }
+        }
+    }
+}
--- prototipos/componentes/seguridad/Prototipo-V2/PROTOTIPO MVC/PrototipoPrincipal/PrototipoPrincipal/Program.cs
+++ prototipos/componentes/seguridad/Prototipo-V2/PROTOTIPO MVC/PrototipoPrincipal/PrototipoPrincipal/Program.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 namespace PrototipoPrincipal
 {
     static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Mostrar Splash
+            using (var splash = new CapaVista.frmSlash())
+            {
+                splash.ShowDialog();
+            }
+
+            // esto muestra el Login despues
+
             Application.Run(new CapaVista.frmLogin());
         }
     }
