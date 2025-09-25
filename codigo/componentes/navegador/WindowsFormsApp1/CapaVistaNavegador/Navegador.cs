b30ded82c4acacd072b42d963980fee7bdfaed96
DanielReyes03 <daniel_reyes2003@outlook.com>
2025-09-24 21:53:26 -0600

    agregando comentarios faltantes

--- codigo/componentes/navegador/WindowsFormsApp1/CapaVistaNavegador/Navegador.cs
+++ codigo/componentes/navegador/WindowsFormsApp1/CapaVistaNavegador/Navegador.cs
@@ -342,6 +342,7 @@ namespace Capa_Vista_Navegador
         }
 
         // Configuracion de data grid view
+        // ======================= Boton para posicionarse en el registro siguiente - Fredy Reyes 0901-22-9800 =======================
         public void configurarDataGridView(ConfiguracionDataGridView configuracion)
         {
             configuracionDataGridView = configuracion;
