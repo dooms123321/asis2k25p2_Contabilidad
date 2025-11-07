b970e1fe5205a86841a5216a09e7e6e690383624
Kenph-Ansonny <ansonny54@gmail.com>
2025-11-07 17:07:32 -0600

    Envio de Polizas Locales recuperadas tras perder commit

---
+++ prototipos/modulos/contabilidad/Polizas Locales/Polizas_LocalesMVC/Capa_Vista_Polizas/Frm_DetallePolizas.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace Capa_Vista_Polizas
+{
+    partial class Frm_DetallePolizas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Salir = new System.Windows.Forms.Button();
+            this.Btn_Refrescar = new System.Windows.Forms.Button();
+            this.Btn_Cancelar = new System.Windows.Forms.Button();
+            this.Btn_Grabar = new System.Windows.Forms.Button();
+            this.Btn_Editar = new System.Windows.Forms.Button();
+            this.Btn_Ingresar = new System.Windows.Forms.Button();
+            this.Lbl_TotalCargo = new System.Windows.Forms.Label();
+            this.Dgv_DetallePoliza = new System.Windows.Forms.DataGridView();
+            this.Lbl_TotalAbono = new System.Windows.Forms.Label();
+            this.Lbl_Diferencial = new System.Windows.Forms.Label();
+            this.Btn_Quitar = new System.Windows.Forms.Button();
+            this.Lbl_DetalleMovimientos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cmb_Tipo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Cmb_CodigoCuenta = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Txt_Valor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Txt_IdPoliza = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Txt_Concepto = new System.Windows.Forms.TextBox();
+            this.Btn_Aceptar = new System.Windows.Forms.Button();
+            this.Dtp_Fecha = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_DetallePoliza)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Salir
+            // 
+            this.Btn_Salir.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Salir.Location = new System.Drawing.Point(584, 12);
+            this.Btn_Salir.Name = "Btn_Salir";
+            this.Btn_Salir.Size = new System.Drawing.Size(100, 80);
+            this.Btn_Salir.TabIndex = 12;
+            this.Btn_Salir.Text = "Salir";
+            this.Btn_Salir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Salir.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Refrescar
+            // 
+            this.Btn_Refrescar.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Refrescar.Location = new System.Drawing.Point(478, 12);
+            this.Btn_Refrescar.Name = "Btn_Refrescar";
+            this.Btn_Refrescar.Size = new System.Drawing.Size(100, 80);
+            this.Btn_Refrescar.TabIndex = 11;
+            this.Btn_Refrescar.Text = "Refrescar";
+            this.Btn_Refrescar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Refrescar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Cancelar
+            // 
+            this.Btn_Cancelar.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Cancelar.Location = new System.Drawing.Point(372, 12);
+            this.Btn_Cancelar.Name = "Btn_Cancelar";
+            this.Btn_Cancelar.Size = new System.Drawing.Size(100, 80);
+            this.Btn_Cancelar.TabIndex = 10;
+            this.Btn_Cancelar.Text = "Cancelar";
+            this.Btn_Cancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Grabar
+            // 
+            this.Btn_Grabar.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Grabar.Location = new System.Drawing.Point(234, 12);
+            this.Btn_Grabar.Name = "Btn_Grabar";
+            this.Btn_Grabar.Size = new System.Drawing.Size(132, 80);
+            this.Btn_Grabar.TabIndex = 9;
+            this.Btn_Grabar.Text = "Grabar";
+            this.Btn_Grabar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Grabar.UseVisualStyleBackColor = true;
+            this.Btn_Grabar.Click += new System.EventHandler(this.Btn_Grabar_Click);
+            // 
+            // Btn_Editar
+            // 
+            this.Btn_Editar.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Editar.Location = new System.Drawing.Point(128, 12);
+            this.Btn_Editar.Name = "Btn_Editar";
+            this.Btn_Editar.Size = new System.Drawing.Size(100, 80);
+            this.Btn_Editar.TabIndex = 8;
+            this.Btn_Editar.Text = "Editar";
+            this.Btn_Editar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Editar.UseVisualStyleBackColor = true;
+            this.Btn_Editar.Click += new System.EventHandler(this.Btn_Editar_Click);
+            // 
+            // Btn_Ingresar
+            // 
+            this.Btn_Ingresar.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Ingresar.Location = new System.Drawing.Point(22, 12);
+            this.Btn_Ingresar.Name = "Btn_Ingresar";
+            this.Btn_Ingresar.Size = new System.Drawing.Size(100, 80);
+            this.Btn_Ingresar.TabIndex = 7;
+            this.Btn_Ingresar.Text = "Ingresar";
+            this.Btn_Ingresar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Ingresar.UseVisualStyleBackColor = true;
+            this.Btn_Ingresar.Click += new System.EventHandler(this.Btn_Ingresar_Click);
+            // 
+            // Lbl_TotalCargo
+            // 
+            this.Lbl_TotalCargo.AutoSize = true;
+            this.Lbl_TotalCargo.Location = new System.Drawing.Point(19, 689);
+            this.Lbl_TotalCargo.Name = "Lbl_TotalCargo";
+            this.Lbl_TotalCargo.Size = new System.Drawing.Size(78, 16);
+            this.Lbl_TotalCargo.TabIndex = 13;
+            this.Lbl_TotalCargo.Text = "Total Cargo";
+            // 
+            // Dgv_DetallePoliza
+            // 
+            this.Dgv_DetallePoliza.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_DetallePoliza.Location = new System.Drawing.Point(22, 368);
+            this.Dgv_DetallePoliza.Name = "Dgv_DetallePoliza";
+            this.Dgv_DetallePoliza.RowHeadersWidth = 51;
+            this.Dgv_DetallePoliza.RowTemplate.Height = 24;
+            this.Dgv_DetallePoliza.Size = new System.Drawing.Size(898, 285);
+            this.Dgv_DetallePoliza.TabIndex = 14;
+            this.Dgv_DetallePoliza.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dgv_DetallePoliza_CellClick);
+            // 
+            // Lbl_TotalAbono
+            // 
+            this.Lbl_TotalAbono.AutoSize = true;
+            this.Lbl_TotalAbono.Location = new System.Drawing.Point(477, 689);
+            this.Lbl_TotalAbono.Name = "Lbl_TotalAbono";
+            this.Lbl_TotalAbono.Size = new System.Drawing.Size(81, 16);
+            this.Lbl_TotalAbono.TabIndex = 15;
+            this.Lbl_TotalAbono.Text = "Total Abono";
+            // 
+            // Lbl_Diferencial
+            // 
+            this.Lbl_Diferencial.AutoSize = true;
+            this.Lbl_Diferencial.Location = new System.Drawing.Point(921, 689);
+            this.Lbl_Diferencial.Name = "Lbl_Diferencial";
+            this.Lbl_Diferencial.Size = new System.Drawing.Size(71, 16);
+            this.Lbl_Diferencial.TabIndex = 16;
+            this.Lbl_Diferencial.Text = "Diferencial";
+            // 
+            // Btn_Quitar
+            // 
+            this.Btn_Quitar.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Quitar.Location = new System.Drawing.Point(940, 368);
+            this.Btn_Quitar.Name = "Btn_Quitar";
+            this.Btn_Quitar.Size = new System.Drawing.Size(100, 80);
+            this.Btn_Quitar.TabIndex = 17;
+            this.Btn_Quitar.Text = "Quitar";
+            this.Btn_Quitar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Quitar.UseVisualStyleBackColor = true;
+            this.Btn_Quitar.Click += new System.EventHandler(this.Btn_Quitar_Click_1);
+            // 
+            // Lbl_DetalleMovimientos
+            // 
+            this.Lbl_DetalleMovimientos.AutoSize = true;
+            this.Lbl_DetalleMovimientos.Location = new System.Drawing.Point(19, 336);
+            this.Lbl_DetalleMovimientos.Name = "Lbl_DetalleMovimientos";
+            this.Lbl_DetalleMovimientos.Size = new System.Drawing.Size(148, 16);
+            this.Lbl_DetalleMovimientos.TabIndex = 18;
+            this.Lbl_DetalleMovimientos.Text = "Detalle de Movimientos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 16);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Detalle de Movimientos";
+            // 
+            // Cmb_Tipo
+            // 
+            this.Cmb_Tipo.FormattingEnabled = true;
+            this.Cmb_Tipo.Location = new System.Drawing.Point(591, 275);
+            this.Cmb_Tipo.Name = "Cmb_Tipo";
+            this.Cmb_Tipo.Size = new System.Drawing.Size(152, 24);
+            this.Cmb_Tipo.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(622, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Cargo/Abono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Codigo Cuenta";
+            // 
+            // Cmb_CodigoCuenta
+            // 
+            this.Cmb_CodigoCuenta.FormattingEnabled = true;
+            this.Cmb_CodigoCuenta.Location = new System.Drawing.Point(31, 275);
+            this.Cmb_CodigoCuenta.Name = "Cmb_CodigoCuenta";
+            this.Cmb_CodigoCuenta.Size = new System.Drawing.Size(554, 24);
+            this.Cmb_CodigoCuenta.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(829, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Valor";
+            // 
+            // Txt_Valor
+            // 
+            this.Txt_Valor.Location = new System.Drawing.Point(749, 275);
+            this.Txt_Valor.Name = "Txt_Valor";
+            this.Txt_Valor.Size = new System.Drawing.Size(171, 22);
+            this.Txt_Valor.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 16);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Docto. Poliza";
+            // 
+            // Txt_IdPoliza
+            // 
+            this.Txt_IdPoliza.Location = new System.Drawing.Point(114, 120);
+            this.Txt_IdPoliza.Name = "Txt_IdPoliza";
+            this.Txt_IdPoliza.Size = new System.Drawing.Size(131, 22);
+            this.Txt_IdPoliza.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(267, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 16);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Emision (fecha)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 16);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Concepto";
+            // 
+            // Txt_Concepto
+            // 
+            this.Txt_Concepto.Location = new System.Drawing.Point(97, 173);
+            this.Txt_Concepto.Name = "Txt_Concepto";
+            this.Txt_Concepto.Size = new System.Drawing.Size(823, 22);
+            this.Txt_Concepto.TabIndex = 31;
+            // 
+            // Btn_Aceptar
+            // 
+            this.Btn_Aceptar.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Aceptar.Location = new System.Drawing.Point(940, 219);
+            this.Btn_Aceptar.Name = "Btn_Aceptar";
+            this.Btn_Aceptar.Size = new System.Drawing.Size(100, 80);
+            this.Btn_Aceptar.TabIndex = 32;
+            this.Btn_Aceptar.Text = "Aceptar";
+            this.Btn_Aceptar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Aceptar.UseVisualStyleBackColor = true;
+            this.Btn_Aceptar.Click += new System.EventHandler(this.Btn_Aceptar_Click);
+            // 
+            // Dtp_Fecha
+            // 
+            this.Dtp_Fecha.Location = new System.Drawing.Point(372, 123);
+            this.Dtp_Fecha.Name = "Dtp_Fecha";
+            this.Dtp_Fecha.Size = new System.Drawing.Size(200, 22);
+            this.Dtp_Fecha.TabIndex = 33;
+            // 
+            // Frm_DetallePolizas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 741);
+            this.Controls.Add(this.Dtp_Fecha);
+            this.Controls.Add(this.Btn_Aceptar);
+            this.Controls.Add(this.Txt_Concepto);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Txt_IdPoliza);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Txt_Valor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Cmb_CodigoCuenta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Cmb_Tipo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_DetalleMovimientos);
+            this.Controls.Add(this.Btn_Quitar);
+            this.Controls.Add(this.Lbl_Diferencial);
+            this.Controls.Add(this.Lbl_TotalAbono);
+            this.Controls.Add(this.Dgv_DetallePoliza);
+            this.Controls.Add(this.Lbl_TotalCargo);
+            this.Controls.Add(this.Btn_Salir);
+            this.Controls.Add(this.Btn_Refrescar);
+            this.Controls.Add(this.Btn_Cancelar);
+            this.Controls.Add(this.Btn_Grabar);
+            this.Controls.Add(this.Btn_Editar);
+            this.Controls.Add(this.Btn_Ingresar);
+            this.Name = "Frm_DetallePolizas";
+            this.Text = "Frm_DetallePolizas";
+            this.Load += new System.EventHandler(this.Frm_DetallePolizas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_DetallePoliza)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Salir;
+        private System.Windows.Forms.Button Btn_Refrescar;
+        private System.Windows.Forms.Button Btn_Cancelar;
+        private System.Windows.Forms.Button Btn_Grabar;
+        private System.Windows.Forms.Button Btn_Editar;
+        private System.Windows.Forms.Button Btn_Ingresar;
+        private System.Windows.Forms.Label Lbl_TotalCargo;
+        private System.Windows.Forms.DataGridView Dgv_DetallePoliza;
+        private System.Windows.Forms.Label Lbl_TotalAbono;
+        private System.Windows.Forms.Label Lbl_Diferencial;
+        private System.Windows.Forms.Button Btn_Quitar;
+        private System.Windows.Forms.Label Lbl_DetalleMovimientos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Cmb_Tipo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox Cmb_CodigoCuenta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Txt_Valor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Txt_IdPoliza;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Txt_Concepto;
+        private System.Windows.Forms.Button Btn_Aceptar;
+        private System.Windows.Forms.DateTimePicker Dtp_Fecha;
+    }
+}
